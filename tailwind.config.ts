1aaf92203c2ca4a9c9b98eaa04ed320cf24dbf20
Rishika <rishikaofficial2024@gmail.com>
2026-06-24 09:34:36 +0000

    check ui error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,10 +74,16 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'progress': {
+          '0%': { width: '0%', marginLeft: '0%' },
+          '50%': { width: '50%', marginLeft: '25%' },
+          '100%': { width: '100%', marginLeft: '0%' }
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'progress': 'progress 2s infinite ease-in-out'
       },
     },
   },
